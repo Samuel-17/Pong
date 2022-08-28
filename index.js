e99d0bbd0bf80de816f041a4061a684085b41f82
Samuel-17 <93955414+Samuel-17@users.noreply.github.com>
2022-08-28 14:43:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,300 @@
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+//Sons de bola de tênis
+const audio = new Audio('Tênis.mp3');
+
+//Código da tecla
+let keyPress;
+
+//Status de movimentação
+let pressed = {
+    top: false,
+    bottom: false
+};
+
+//Dimensões canvas
+let width = canvas.width;
+let height = canvas.height;
+
+// Classe do player do usuário
+class Player {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.width = 10;
+        this.height = 50;
+    }
+
+    draw() {
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+};
+
+//Classe da bola
+class Ball {
+    constructor() {
+        this.position = {
+            x: 505,
+            y: 225
+        }
+
+        this.width = 15;
+        this.height = 15;
+    }
+
+    draw() {
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+};
+
+//Classe da IA do jogo
+class Ia {
+    constructor() {
+        this.position = {
+            x: 890,
+            y: 0
+        }
+
+        this.width = 10;
+        this.height = 50;
+    }
+
+    draw() {
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+//Demarcação do campo
+class Line {
+    constructor() {
+        this.position = {
+            x: 440,
+            y: 0
+        }
+
+        this.width = 2;
+        this.height = 500;
+    }
+
+    draw() {
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+//Player do usuário
+const player = new Player();
+//IA
+const iaplayer = new Ia();
+//Bola
+const ball = new Ball();
+//Linha central
+const line = new Line();
+
+
+//Aceleração da bola y
+let a = -2;
+
+// Sentido vetorial
+let vector = 'left';
+
+
+//Pontuação
+let score = {
+    player1: 0,
+    player2: 0
+}
+
+
+function scoreBoard() {
+    ctx.strokeStyle = '#ffff'
+    ctx.font = "60px Silkscreen, cursive";
+    ctx.fillText(`${score.player1}  ${score.player2}`, 370, 55);
+}
+
+
+function boundaryArea() {
+    //Movimentação do personagem do player
+    if (pressed.top && player.position.y > 0) {
+        player.position.y -= 10
+    }
+
+    if (pressed.bottom && player.position.y < 450) {
+        player.position.y += 10;
+    }
+}
+
+function ballRoll() {
+    if (vector == "left") {
+        ball.position.x -= 5;
+    } else if (vector == 'right') {
+        ball.position.x += 5
+    } else if (vector == 'stop') {
+        ball.position.x = 450;
+    }
+}
+
+//Função de desenho
+function draw() {
+
+    //Limpar tela
+    ctx.clearRect(0, 0, width, height);
+
+    //Chmando função de desenho das classes
+    line.draw();
+    player.draw();
+    iaplayer.draw();
+    ball.draw();
+
+    // Placar de pontuação
+    scoreBoard();
+
+    //Movimentação do player no campo
+    boundaryArea();
+
+    //Sentido de rolágem da bola
+    ballRoll();
+
+    //Sentido ininicial da bola
+    ball.position.y += a;
+
+    //Colisões
+    collider();
+
+    //Marcação de pontos
+    Win();
+
+    //Movimentação da IA
+    if (ball.position.x > 500) {
+        movimentationIA()
+    }
+
+    requestAnimationFrame(draw);
+}
+
+function movimentationIA() {
+    if (ball.position.y >= iaplayer.position.y && ball.position.y <= 425) {
+        iaplayer.position.y += 5
+    }
+    if (ball.position.y <= iaplayer.position.y > 0) {
+        iaplayer.position.y -= 5
+    }
+};
+
+function collider() {
+    if (ball.position.x + ball.width >= iaplayer.position.x &&
+        ball.position.x <= iaplayer.position.x + iaplayer.width &&
+        ball.position.y + ball.height >= iaplayer.position.y &&
+        ball.position.y <= iaplayer.position.y + iaplayer.height) {
+        //Mudando sentido de rolagem da bola para +
+        vector = "left"
+        audioActive()
+    }
+
+    //Colisão com o jogador do usuário
+    if (ball.position.x + ball.width >= player.position.x &&
+        ball.position.x <= player.position.x + player.width &&
+        ball.position.y + ball.height >= player.position.y &&
+        ball.position.y <= player.position.y + player.height) {
+        //Mudando sentido de rolagem da bola para +
+        vector = "right"
+        audioActive()
+    }
+
+    //Colisão da bola no top
+    if (ball.position.y <= 0) {
+        a = a * (-1);
+        audioActive()
+    }
+    //Colisão da bola no bottom
+    if (ball.position.y >= height - 20) {
+        a = a * (-1);
+        audioActive()
+    }
+}
+
+function audioActive() {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function Win() {
+    if (ball.position.x <= -1) {
+        contScore(0);
+    }
+
+    if (ball.position.x >= width - 20) {
+        contScore(1);
+    }
+}
+
+function contScore(x) {
+    if (x) {
+        ball.position.x = 450;
+        vector = 'left';
+        (score.player1 == 5 ? location.reload() : score.player1++);
+        return
+    } else {
+        ball.position.x = 450
+        vector = 'right';
+        (score.player2 == 4 ? location.reload() : score.player2++);
+        return
+    }
+}
+
+//Captura de teclas I/O
+document.addEventListener('keydown', (e) => {
+    keyPress = e.keyCode;
+    switch (keyPress) {
+
+        //arrow up
+        case 38:
+            return pressed = {
+                top: true,
+                bottom: false
+            }
+            break;
+
+        //arrow down
+        case 40:
+            return pressed = {
+                top: false,
+                bottom: true
+            }
+            break;
+
+        // W
+        case 87:
+            return pressed = {
+                top: true,
+                bottom: false
+            }
+            break;
+
+        //S
+        case 83:
+            return pressed = {
+                top: false,
+                bottom: true
+            }
+            break;
+    }
+});
+
+//Captura de soltagem da tecla
+document.addEventListener('keyup', (e) => {
+    return pressed = {
+        top: false,
+        bottom: false
+    };
+});
+
+draw();
